51ebad148534c14da2e392e19f8b218dcfc5818f
bensinober <bensinober@gmail.com>
2024-04-04 00:07:02 +0200

    Add prelim win build target

--- build.zig
+++ build.zig
@@ -15,27 +15,12 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "shell-game",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
     // Compile static library of opencv
     const cv = b.addStaticLibrary(std.Build.StaticLibraryOptions{
         .name = "opencv",
         .target = target,
         .optimize = optimize,
     });
-    const c_build_options: []const []const u8 = &.{
-        "-Wall",
-        "-Wextra",
-        "-std=c++11",
-        "-stdlib=libc++",
-    };
     cv.addIncludePath(.{ .path = "include" });
     cv.addIncludePath(.{ .path = "include/contrib" });
     cv.addLibraryPath(.{ .path = "libs" });
@@ -58,11 +43,68 @@ pub fn build(b: *std.Build) void {
         "libs/video.cpp",
         "libs/videoio.cpp",
         "libs/contrib/tracking.cpp",
-    }, c_build_options);
+    }, &[_][]const u8{
+        "-Wall",
+        "-Wextra",
+        "-std=c++11",
+        "-stdlib=libc++",
+    });
 
     cv.linkLibC();
     cv.linkLibCpp();
     b.installArtifact(cv);
+
+    // Compile static library of opencv for win64
+    const wincv = b.addStaticLibrary(std.Build.StaticLibraryOptions{
+        .name = "opencv",
+        .target = std.zig.CrossTarget{
+            .cpu_arch = .x86_64,
+            .os_tag = .windows,
+        },
+        .optimize = optimize,
+    });
+    wincv.addIncludePath(.{ .path = "include" });
+    wincv.addIncludePath(.{ .path = "include/contrib" });
+    wincv.addLibraryPath(.{ .path = "libs" });
+    wincv.addLibraryPath(.{ .path = "libs/contrib" });
+    wincv.addIncludePath(.{ .path = "/usr/local/include" });
+    wincv.addIncludePath(.{ .path = "/usr/local/include/opencv4" });
+    wincv.addCSourceFiles(&.{
+        "libs/asyncarray.cpp",
+        "libs/calib3d.cpp",
+        "libs/core.cpp",
+        "libs/dnn.cpp",
+        "libs/features2d.cpp",
+        "libs/highgui.cpp",
+        "libs/imgcodecs.cpp",
+        "libs/imgproc.cpp",
+        "libs/objdetect.cpp",
+        "libs/photo.cpp",
+        "libs/svd.cpp",
+        "libs/version.cpp",
+        "libs/video.cpp",
+        "libs/videoio.cpp",
+        "libs/contrib/tracking.cpp",
+    }, &[_][]const u8{
+        "-Wall",
+        "-Wextra",
+        "-std=c++11",
+    });
+
+    wincv.linkLibC();
+    wincv.linkLibCpp();
+    b.installArtifact(wincv);
+    // target host compiler
+
+    const exe = b.addExecutable(.{
+        .name = "robot-eyes",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
 
     exe.addAnonymousModule("zigcv", .{ .source_file = .{ .path = "libs/zigcv.zig" } });
 
@@ -70,21 +112,89 @@ pub fn build(b: *std.Build) void {
     const wsMod = b.dependency("websocket", .{ .target = target, .optimize = optimize});
     exe.addModule("websocket", wsMod.module("websocket"));
 
-
     exe.linkLibrary(cv);
     exe.addIncludePath(.{ .path = "include" });
     exe.addIncludePath(.{ .path = "include/contrib" });
     exe.addIncludePath(.{ .path = "/usr/local/include" });
     exe.addIncludePath(.{ .path = "/usr/local/include/opencv4" });
+    exe.linkLibC();
     exe.linkSystemLibrary("opencv4");
+    exe.linkSystemLibrary("simpleble-c");
     exe.linkSystemLibrary("unwind");
     exe.linkSystemLibrary("m");
     exe.linkSystemLibrary("c");
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
+
+    // target armv7 - raspberry PI
+    // const arm = b.addExecutable(.{
+    //     .name = "robot-eyes-arm",
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = std.zig.CrossTarget{
+    //         .cpu_arch = .aarch64,
+    //         .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a72 },
+    //         .os_tag = .linux,
+    //     },
+    //     .optimize = optimize,
+    // });
+    // arm.linkLibrary(cv);
+    // arm.linkLibC();
+    // arm.addLibraryPath(.{ .path = "lib/armv7"});
+    // arm.addIncludePath(.{ .path = "include" });
+    // arm.addIncludePath(.{ .path = "include/contrib" });
+    // arm.linkSystemLibrary("simpleble-c");
+    // arm.linkSystemLibrary("opencv4");
+    // arm.linkSystemLibrary("unwind");
+    // arm.linkSystemLibrary("m");
+    // arm.addAnonymousModule("zigcv", .{ .source_file = .{ .path = "libs/zigcv.zig" } });
+    // arm.addModule("websocket", wsMod.module("websocket"));
+    // _ = b.installArtifact(arm);
+
+    // target win64
+    // const win = b.addExecutable(.{
+    //     .name = "robot-eyes-win",
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = std.zig.CrossTarget{
+    //         .cpu_arch = .x86_64,
+    //         .os_tag = .windows,
+    //         //.abi = .msvc, // .gnu
+    //     },
+    //     .optimize = optimize,
+    // });
+    // win.linkLibC();
+    // win.linkSystemLibrary("unwind");
+    // win.linkSystemLibrary("m");
+    // win.addIncludePath(.{ .path = "include" });
+    // win.addIncludePath(.{ .path = "include/contrib" });
+
+    // // mingw
+    // win.addLibraryPath(.{ .path = "/usr/x86_64-w64-mingw32"});
+    // win.addIncludePath(.{ .path = "include/win64/mingw64" });
+
+    // // Opencv4
+    // win.linkLibrary(wincv); // we add statick object instead
+    // win.addLibraryPath(.{ .path = "libs/win64/opencv4/static"});
+    // win.addObjectFile(.{ .path = "zig-out/lib/opencv-win.lib"});
+    // win.addObjectFile(.{ .path = "libs/win64/opencv4/opencv_world490.dll"});
+    // win.linkSystemLibrary("opencv4");
+
+    // // SimpleBLE
+    // win.addIncludePath(.{ .path = "include/simpleble-c" });
+    // win.addLibraryPath(.{ .path = "libs/win64/simpleble"});
+    // win.addObjectFile(.{ .path = "libs/win64/simpleble/simpleble-c.dll"});
+    // win.linkSystemLibraryName("simpleble-c");
+    // // // mingw
+    // // win.addLibraryPath(.{ .path = "libs/win64/mingw64" });
+    // // //win.addLibraryPath(.{ .path = "libs/win64/mingw/install/staticlib" });
+    // // win.addIncludePath(.{ .path = "include/install" });
+    // // //win.addIncludePath("c:/opencv/build/install/include");
+    // // //win.addLibraryPath("c:/opencv/build/install/x64/mingw/staticlib");
+
+
+    // // win.addObjectFile(.{ .path = "libs/win64/mingw64/libstdc++-6.dll"});
+    // win.addAnonymousModule("zigcv", .{ .source_file = .{ .path = "libs/zigcv.zig" } });
+    // win.addModule("websocket", wsMod.module("websocket"));
+    // _ = b.installArtifact(win);
 
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
